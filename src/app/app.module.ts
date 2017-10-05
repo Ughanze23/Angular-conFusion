b04ab83f913197acc507efc2a709604766cfbf3c
Ughanze Ikenna <ughanzepolycarp@gmail.com>
2017-10-05 08:06:29 -1100

    angular components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,25 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { MdToolbarModule } from '@angular/material';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { AppComponent } from './app.component';
+import {MatListModule} from '@angular/material';
+
+
 
 import 'hammerjs';
+import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MdToolbarModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
