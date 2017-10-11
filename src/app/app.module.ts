04f160a3b54d316cf0e706946829300c87dc0f20
Ughanze Ikenna <ughanzepolycarp@gmail.com>
2017-10-10 23:57:11 -1100

    Single page application part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,17 +15,26 @@ import 'hammerjs';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
-
-import {DishService} from './services/dish.service';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+
+import {DishService} from './services/dish.service';
+import {PromotionService} from './services/promotion.service';
+
+import {AppRoutingModule} from './app-routing/app-routing.module';
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
     DishdetailComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
@@ -35,9 +44,10 @@ import { FooterComponent } from './footer/footer.component';
     MatListModule,
     MatGridListModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    AppRoutingModule
   ],
-  providers: [DishService],
+  providers: [DishService, PromotionService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/shared/dish.ts
+++ src/app/shared/dish.ts
@@ -1,11 +1,12 @@
 import { Comment } from './comments';
 export class Dish {
-
+    id: number;
     name: string;
     image: string;
     category: string;
     label: string;
     price: string;
+    featured: boolean;
     description: string;
     comments: Comment[];
 
